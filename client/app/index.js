30cb472e5e70e8ccec38eb2b9d940d66add9d9fc
James Mitchell <jamesmitchell@Jamess-MacBook-Air.local>
2017-03-16 17:20:57 -0500

    fixed syntax errors

--- client/app/index.js
+++ client/app/index.js
@@ -1,3 +1,3 @@
 ReactDOM.render(
-  <App ={window.searchgithub} API_KEY={window.github_API_KEY}/>, document.getElementById('app')
+  <App ={window.searchGitHub} />, document.getElementById('app')
   );
