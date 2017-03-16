159eff41a33d11b449f2d32a9477925526819d9f
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-16 18:14:06 -0400

    Create Nav

--- client/Nav.jsx
+++ client/Nav.jsx
@@ -2,7 +2,7 @@ var Nav = () => (
 <nav className='navbar navbar-inverse'>
   <div className='container-fluid'>
     <div className='navbar-header'>
-      <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
+      <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#nav-collapse-1' aria-expanded='false'>
         <span className='sr-only'>Toggle navigation</span>
         <span className='icon-bar'></span>
         <span className='icon-bar'></span>
@@ -11,10 +11,13 @@ var Nav = () => (
       <a className="navbar-brand" href="#/"><img src="assets/logo.png" />Git It Together</a>
     </div>
 
-    <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+    <div className='collapse navbar-collapse' id='nav-collapse-1'>
       <ul className='nav navbar-nav'>
-        <li className="active"><a href='#/open'>Open <span className='sr-only'>(current)</span></a></li>
-        <li><a href='#/archive'>Archive</a></li>
+      {/*
+        <li><Link to="/dashboard" activeClassName="active sr-only">Dashboard</Link></li>
+        <li><Link to="/projects" activeClassName="active sr-only">Projects</Link></li>
+        <li><Link to="/logout">Sign Out</Link></li>
+      */}
       </ul>
     </div>
   </div>
