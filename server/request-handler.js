e0bbc6d3667c8d2ee2b54450eae990390a429b11
lmegviar <lmegviar@gmail.com>
2017-03-16 14:56:55 -0700

    Add get project list route and handler

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,13 +4,22 @@ exports.addProject = function (req, res) {
   console.log('addProjects ran! Request:', req.body);
   res.end();
 };
+/*
+req.body: {githubHandle: 'Example', repoName: 'example_repo}
+res: {}
+*/
+
+exports.listProjects = function (req, res) {
+  console.log('listProjects ran! Request:', req.body);
+  res.end();
+};
 
 exports.addResource = function (req, res) {
   console.log('addResources ran! Request:', req.body);
   res.end();
 };
 
-exports.getProject = function (res, res) {
+exports.getProject = function (req, res) {
   console.log('getProject ran! Request:', req.body);
   res.end();
 };
