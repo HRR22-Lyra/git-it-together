34b6a962b178c3fe9638c162a64f89c7d7802f12
lmegviar <lmegviar@gmail.com>
2017-03-16 12:19:36 -0700

    Add gitignore and routes file

---
+++ server/routes.js
@@ -0,0 +1,8 @@
+var requestHandler = require('./request-handler.js');
+var express = require('express')
+
+module.exports = function (app, express) {
+  app.post('/api/projectList', requestHandler.addProject);
+  app.post('/api/resources', requestHandler.addResource);
+  app.get('/api/project', requestHandler.getProject);
+};
