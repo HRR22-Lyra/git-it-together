154167bae6a6cc4a034e29a35edaea29e646aa63
James Mitchell <jamesmitchell@Jamess-MacBook-Air.local>
2017-03-16 14:22:48 -0500

    added search.jsx

---
+++ client/app/Search.jsx
@@ -0,0 +1,33 @@
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  //create click event
+  handleInputChange(e) {
+    this.props.handleSearchInputChange(e.target.value);
+    this.setState({
+      value: e.target.value
+    });
+  }
+  render () {
+    return (
+      <div className="search-bar form-inline">
+        <input
+        className="form-control"
+        type='text'
+        value={this.state.value}
+        onChange={this.handleInputChange.bind(this)}
+        />
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    );
+  }
+}
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.Search = Search;
