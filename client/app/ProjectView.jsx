97c17381af8365121d14c576d30e0e372f62f69d
James Mitchell <jamesmitchell@Jamess-MacBook-Air.local>
2017-03-16 14:19:20 -0500

    added to client

--- client/app/ProjectView.jsx
+++ client/app/ProjectView.jsx
@@ -1,21 +1,18 @@
-var VideoPlayer = ({video}) => (
+var Project = ({project}) => (
 
-  !video ? <div class="video-player">No video found</div> :
+  !project ? <div class="project-view">No project found</div> :
 
-  <div className="video-player">
+  <div className="project-view">
 
-    <div className="embed-responsive embed-responsive-16by9">
-
-      <iframe className="embed-responsive-item" src={`https://
-        www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
+      <div className="show-project-item" src={`https://github.com/HRR22-Lyra/git-it-together`} allowFullScreen></div>
 
     </div>
 
-    <div className="video-player-details">
+    <div className="project-details">
 
-      <h3>{video.snippet.title}</h3>
+      <h3>{project.snippet.title}</h3>
 
-      <div>{video.snippet.description}</div>
+      <div>{project.snippet.description}</div>
 
     </div>
 
@@ -25,10 +22,10 @@ var VideoPlayer = ({video}) => (
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoPlayer.propTypes = {
-  video: React.PropTypes.object.isRequired
+Project.propTypes = {
+  project: React.PropTypes.object.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.VideoPlayer = VideoPlayer;
+window.Project = Project;
