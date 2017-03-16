73df939fab871057b3cb5c75464bc4527d5fbc36
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-16 18:32:20 -0400

    Create basic project list entry

---
+++ client/ProjectListEntry.jsx
@@ -0,0 +1,24 @@
+var ProjectListEntry = ({project, handleProjectListEntryTitleClick}) => (
+  <div className="project-list-entry"  onClick={ () => { handleProjectListEntryClick(project); } } >
+  {/*
+    <div className="media-left media-middle">
+      <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
+    </div>
+  */}
+    <div className="entry-body">
+      <div className="project-list-entry-title">{project.title}</div>
+      <div className="project-list-entry-detail">{project.description}</div>
+    </div>
+  </div>
+);
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+ProjectListEntry.propTypes = {
+  project: React.PropTypes.object.isRequired
+};
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.ProjectListEntry = ProjectListEntry;
+
