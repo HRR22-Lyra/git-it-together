deafc51a4c992ee458765f302d3a08007b159479
lmegviar <lmegviar@gmail.com>
2017-03-16 14:04:16 -0700

    Update SSL option for database

--- server/db_config.js
+++ server/db_config.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 
 //connect to Heroku PostGres Database
-var db = new Sequelize("postgres://xhzrrvvawqzqov:1c2ac54a72b223e5d603ce03d8195194ac39336f544c04b266758f083aea4a15@ec2-23-23-228-115.compute-1.amazonaws.com:5432/d5lfn726hf4pp6");
+var db = new Sequelize("postgres://xhzrrvvawqzqov:1c2ac54a72b223e5d603ce03d8195194ac39336f544c04b266758f083aea4a15@ec2-23-23-228-115.compute-1.amazonaws.com:5432/d5lfn726hf4pp6?ssl=true", {"dialect":"postgres", "ssl":true, "dialectOptions":{"ssl":{"require":true}}});
 
 var Project = db.define('Project', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
