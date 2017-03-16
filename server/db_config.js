155d33663f25eb4eb502c8ad30947a3f8c22674e
lmegviar <lmegviar@gmail.com>
2017-03-16 12:02:32 -0700

    Revert to db_config with vars defined and db exported

--- server/db_config.js
+++ server/db_config.js
@@ -3,13 +3,13 @@ var Sequelize = require('sequelize');
 //connect to Heroku PostGres Database
 var db = new Sequelize("postgres://xhzrrvvawqzqov:1c2ac54a72b223e5d603ce03d8195194ac39336f544c04b266758f083aea4a15@ec2-23-23-228-115.compute-1.amazonaws.com:5432/d5lfn726hf4pp6");
 
-exports.Project = db.define('Project', {
+var Project = db.define('Project', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   owner: Sequelize.STRING,
   get_repo: Sequelize.STRING
 });
 
-exports.Resource = db.define('Resource', {
+var Resource = db.define('Resource', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   project_id: Sequelize.INTEGER,
   user: Sequelize.STRING,
@@ -18,7 +18,7 @@ exports.Resource = db.define('Resource', {
 
 Resources.belongsTo(Project, {foreignKey: 'project_id'});
 
-exports.Deliverable = db.define('Deliverable', {
+var Deliverable = db.define('Deliverable', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   project_id: Sequelize.INTEGER,
   owner: Sequelize.STRING,
@@ -30,7 +30,7 @@ exports.Deliverable = db.define('Deliverable', {
 
 Deliverables.belongsTo(Project, {foreignKey: 'project_id'});
 
-exports.User = db.define('User', {
+var User = db.define('User', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   github_handle: Sequelize.'STRING'
 });
@@ -38,3 +38,4 @@ exports.User = db.define('User', {
 db.sync();
 //can add force:true to drop tables before recreating them
 
+module.exports = db;
