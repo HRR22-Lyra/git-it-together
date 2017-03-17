c8432a3867a81a9cbb2c5f80e1cbcb851ed6b49a
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-03-17 16:55:28 -0500

    this sucks

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -6,8 +6,7 @@ import {Button} from 'react-bootstrap'
 import Nav from './Nav.jsx';
 import ProjectList from './ProjectList.jsx';
 
-
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { projects: [], currentProject: null, profile: props.auth.getProfile() }
@@ -27,13 +26,11 @@ export default class App extends React.Component {
   }
 
   getGitHubProjects(query) {
-
+    //responsible for grabbing projects
     var options = {
       key: '',
       query: query
-    };
-
-    this.props.searchGitHub()
+    }
   }
   logout(){
     this.props.auth.logout()//add props.auth.on('logged-out') event which should be triggered in authservice.js which refreshes page. and same for logged in or authenticated events rather than the use of routes in authservice and here.
