572cf908ae0d53c747c6248542fa0cd7378a97ae
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-16 20:31:57 -0400

    Add more styles and delete unnecessary space in ProjectView

--- client/app/ProjectListEntryView.jsx
+++ client/app/ProjectListEntryView.jsx
@@ -14,12 +14,8 @@ var ProjectListEntry = ({project, handleProjectListEntryClick}) => (
   </div>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
 ProjectListEntry.propTypes = {
   project: React.PropTypes.object.isRequired
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 window.ProjectListEntry = ProjectListEntry;
