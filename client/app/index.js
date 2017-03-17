2de3ee141cee5dd8df5db7270c4dcfae062f6da1
Kyle Anson <kyle.anson7@gmail.com>
2017-03-17 13:42:42 -0400

    Removal of searchGitHub references which is no longer needed

--- client/app/index.js
+++ client/app/index.js
@@ -1,8 +1,7 @@
-import searchGitHub from '../config/searchGitHub';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(
-  <App searchGitHub={searchGitHub} />, document.getElementById('app')
+  <App />, document.getElementById('app')
   );
