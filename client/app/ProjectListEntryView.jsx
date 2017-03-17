10cf3963dab83cf3bde7cb5755acdf0932fc597a
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-16 22:40:56 -0400

    Fixing merge errors

--- client/app/ProjectListEntryView.jsx
+++ client/app/ProjectListEntryView.jsx
@@ -20,14 +20,6 @@ ProjectListEntry.propTypes = {
   project: React.PropTypes.object.isRequired
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+
 module.exports = ProjectListEntry;
-=======
 window.ProjectListEntry = ProjectListEntry;
->>>>>>> Add more styles and delete unnecessary space in ProjectView
-=======
-window.ProjectListEntry = ProjectListEntry;
->>>>>>> 572cf908ae0d53c747c6248542fa0cd7378a97ae
