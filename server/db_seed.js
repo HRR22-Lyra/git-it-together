b062a052d87d723a6f98a0d6954883975fa64c7a
Kyle Anson <kyle.anson7@gmail.com>
2017-03-20 19:35:09 -0400

    Refactor using bulkCreate Also changed some of the seed data to show possible uses of the table columns

--- server/db_seed.js
+++ server/db_seed.js
@@ -25,39 +25,19 @@ var resourceSeed = [
   {project_id: 1, user: 'w1nd0wz', link: 'https://www.bing.com/'}
 ];
 var deliverableSeed = [
-  {project_id: 1, owner: 'mega_man', status: 'DONE', due_date: 'today', progress: 'none', points: 5},
-  {project_id: 1, owner: 'w1nd0wz', status: 'DONE', due_date: 'today', progress: 'none', points: 15},
-  {project_id: 1, owner: 'mega_man', status: 'DONE', due_date: 'today', progress: 'none', points: 10},
-  {project_id: 1, owner: 'Tre', status: 'DONE', due_date: 'today', progress: 'none', points: 5},
-  {project_id: 1, owner: 'mega_man', status: 'DONE', due_date: 'today', progress: 'none', points: 5}
+  {project_id: 1, owner: 'mega_man', status: 'Done', due_date: '3.19', progress: 'none', points: 5},
+  {project_id: 1, owner: 'w1nd0wz', status: 'Done', due_date: '3.20', progress: 'none', points: 15},
+  {project_id: 1, owner: 'mega_man', status: 'In Progress', due_date: null, progress: 'none', points: 10},
+  {project_id: 1, owner: 'Tre', status: 'Ready', due_date: '4.1', progress: 'none', points: 5},
+  {project_id: 1, owner: 'mega_man', status: 'Backlog', due_date: null, progress: 'none', points: 5}
 ];
 
 // Force sync to drop tables
 db.sync({force: true})
 .then(function() {
-  projectSeed.forEach(function(data) {
-    Project.create({
-      owner: data.owner,
-      get_repo: data.get_repo
-    });
-  });
+  Project.bulkCreate(projectSeed);
 }).then(function() {
-  resourceSeed.forEach(function(data) {
-    Resource.create({
-      project_id: data.project_id,
-      user: data.user,
-      link: data.link
-    });
-  });
+  Resource.bulkCreate(resourceSeed);
 }).then(function() {
-  deliverableSeed.forEach(function(data) {
-    Deliverable.create({
-      project_id: data.project_id,
-      owner: data.owner,
-      status: data.status,
-      due_date: data.due_date,
-      progress: data.progress,
-      points: data.points
-    });
-  });
+  Deliverable.bulkCreate(deliverableSeed);
 });
