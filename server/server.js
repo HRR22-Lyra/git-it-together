6f0880ee2211027558e8ab93e33e6cecc9b627de
lmegviar <lmegviar@gmail.com>
2017-03-20 09:03:16 -0700

    Add socket.io connection and dynamic room capability to backend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// Resources: https://gist.github.com/crtr0/2896891
+// https://github.com/socketio/chat-example/blob/master/index.js
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
@@ -21,11 +24,45 @@ var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Listening on port:', port);
 
+//Socket.io ----------------------------------------------
+
 //Listen for connections to io
 io.on('connection', (socket) => {
-  console.log('A user has connected');
+  socket.on('room', (room) => {
+    //if user is already in a room, leave room
+    if (socket.room) {
+      socket.leave(socket.room);
+      socket.room = room;
+    }
+    socket.join(room);
+    console.log('A user has connected to ' + room);
+    //When a message is sent, emit only to the current room
+    socket.on('message', (message) => {
+      io.to(room).emit(message):
+    });
+  });
   //Listen for disconnects from socket
   socket.on('disconnect', () => {
     console.log ('A user disconnected');
   });
 });
+
+
+/* Client-side code for dynamic room names by project:
+
+set-up a connection between the client and the server
+var socket = io.connect();
+
+// let's assume that the client page, once rendered, knows what room it wants to join
+var room = "abc123";
+
+socket.on('connect', function() {
+   // Connected, let's sign-up for to receive messages for this room
+   socket.emit('room', room);
+});
+
+socket.on('message', function(data) {
+   console.log('Incoming message:', data);
+});
+
+*/
