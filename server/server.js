362bdc72e76bf5f9c1368a2f280bc6de3ed9786b
lmegviar <lmegviar@gmail.com>
2017-03-21 13:56:55 -0700

    Update server to deal with merge conflict remnants

--- server/server.js
+++ server/server.js
@@ -6,17 +6,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var path = require('path');
-
 var http = require('http');
 var app = express();
 var requestHandler = require('./request-handler.js');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-var http = require('http').Server(app);
-var app = express();
-var requestHandler = require('./request-handler.js');
-//a new instance of socket.io is initialized by passing the http object
-var io = require ('socket.io')(http);
 
 // Middleware
 app.use(morgan('dev'));
@@ -56,28 +50,3 @@ io.on('connection', (socket) => {
 var port = process.env.PORT || 8080;
 server.listen(port);
 console.log('Listening on port:', port);
-
-//Soc
-
-//Listen for connections to io
-io.on('connection', (socket) => {
-  socket.on('room', (room) => {
-    //if user is already in a room, leave room
-    if (socket.room) {
-      socket.leave(socket.room);
-      socket.room = room;
-    }
-    socket.join(room);
-    console.log('A user has connected to ' + room);
-    //When a message is sent, emit only to the current room
-    socket.on('message', (message) => {
-      io.to(room).emit(message);
-      //save message to database
-      requestHandler.saveMessage(message);
-    });
-    //Listen for disconnects from socket
-    socket.on('disconnect', () => {
-      console.log ('A user disconnected');
-    });
-  });
-});
