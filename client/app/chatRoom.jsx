73349cdfe4259845f87d9a8b1a8fed4663953e47
lmegviar <lmegviar@gmail.com>
2017-03-21 08:06:41 -0700

    Update chatRoom to include room/project and display most recent messages first

--- client/app/chatRoom.jsx
+++ client/app/chatRoom.jsx
@@ -1,13 +1,14 @@
 // Resources http://beatscodeandlife.ghost.io/react-socket-io-part-i-real-time-chat-application/
 // http://danialk.github.io/blog/2013/06/16/reactjs-and-socket-dot-io-chat-application/
+
+// Insert into outer componenet to render:   <ChatApp user={this.state.profile.nickname} room={this.state.currentProject}></ChatApp>
+
+//TO DO: Set limit on number of messages to display
+
 import React from 'react';
 // import Socket from 'socket.io';
 
 var socket = io.connect();
-
-//Get room/project name from project view
-var room = "Project Name (room name)";
-
 var Message = React.createClass({
   render() {
     return (
@@ -23,7 +24,6 @@ var MessageList = React.createClass({
   render() {
     return (
       <div className='messages'>
-        <h2> Discussion: </h2>
         {
           this.props.messages.map((message, i) => {
             return (
@@ -31,6 +31,7 @@ var MessageList = React.createClass({
                 key={i}
                 user={message.user}
                 text={message.text}
+                room={message.room}
               />
             );
           })
@@ -51,7 +52,7 @@ var MessageForm = React.createClass({
     var message = {
       user : this.props.user,
       text : this.state.text,
-      project: room
+      room: this.props.room
     }
     this.props.onMessageSubmit(message);
     this.setState({ text: '' });
@@ -64,7 +65,7 @@ var MessageForm = React.createClass({
   render() {
     return(
       <div className='message_form'>
-        <h3>Join the conversation:</h3>
+        <h3>Chat about {this.props.room}</h3>
         <form onSubmit={this.handleSubmit}>
           <input
             onChange={this.changeHandler}
@@ -87,19 +88,19 @@ var ChatApp = React.createClass({
     socket.on('init', this._initialize);
     socket.on('message', this._messageRecieve);
     socket.on('connect', function() {
-       socket.emit('room', room);
+       socket.emit('room', this.props.room);
     });
   },
 
   _messageRecieve(message) {
     var {messages} = this.state;
-    messages.push(message);
+    messages.unshift(message);
     this.setState({messages});
   },
 
   handleMessageSubmit(message) {
     var {messages} = this.state;
-    messages.push(message);
+    messages.unshift(message);
     this.setState({messages});
     socket.emit('message', message);
   },
@@ -107,12 +108,13 @@ var ChatApp = React.createClass({
   render() {
     return (
       <div>
-        <MessageList
-          messages={this.state.messages}
-        />
         <MessageForm
           onMessageSubmit={this.handleMessageSubmit}
           user={this.props.user}
+          room={this.props.room}
+        />
+        <MessageList
+          messages={this.state.messages}
         />
       </div>
     );
