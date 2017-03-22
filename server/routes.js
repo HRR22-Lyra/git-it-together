d9e610c7b37a1540a5bfdc2b6b8761a69e9c7981
Kyle Anson <kyle.anson7@gmail.com>
2017-03-22 17:23:07 -0400

    Clean weird merge conflict residue

--- server/routes.js
+++ server/routes.js
@@ -7,9 +7,6 @@ module.exports = function (app, express) {
   app.post('/api/resources', requestHandler.addResource);
   app.post('/api/project', requestHandler.fetchProject);
   app.post('/api/deliverables', requestHandler.addDeliverable);
-<<<<<<< HEAD
-  app.post('/api/listRepos', requestHandler.listRepos);
-=======
   app.get('/api/deliverables', requestHandler.listDeliverables);
->>>>>>> Pull in deliverables from database before rerendering the project view
+  app.post('/api/listRepos', requestHandler.listRepos);
 };
