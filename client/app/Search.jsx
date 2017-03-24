28addab45c7e38cb2c318b6b2dbc7c84ab1c13a2
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-24 02:45:18 -0400

    Refactor breakpoints to allow indefinite scalability

--- client/app/Search.jsx
+++ client/app/Search.jsx
@@ -29,26 +29,26 @@ var Search = React.createClass({
         return (
           <div className="container-fluid">
             <div className="project-list">
-              <div className="row">
-                <div className="col project-list-header">
-                  <h1>Projects</h1>
-                  <hr />
-                  <div className="project-list-search">
-                    <div className="row">
-                    <div className="col">
-                      <form className="form search-repo">
-                          <input type="text" value={this.state.searchString} onChange={this.handleChange}  className="form-control" id="search-repo" placeholder="Search your project list" />
-                      </form>
-                    </div>
-                    <div className="col">
+                  <div className="row">
+                    <div className="col project-list-header">
+                      <h1>Projects</h1>
+                      <hr />
+                      <div className="project-list-search">
+                        <div className="row">
+                        <div className="col">
+                          <form className="form search-repo">
+                              <input type="text" value={this.state.searchString} onChange={this.handleChange}  className="form-control" id="search-repo" placeholder="Search your project list" />
+                          </form>
+                        </div>
+                        <div className="col">
                           <form className="form-inline search-repo">
                               <UserRepos handleRepoClick={this.props.handleRepoClick.bind(this)}/>
                           </form>
+                        </div>
+                      </div>
+                      </div>
                     </div>
                   </div>
-                  </div>
-                </div>
-              </div>
               {/* DO NOT change this to className. I don't know why but this is just the way it has to be... */}
               <div class="row">
                 <div className="projects-section">
