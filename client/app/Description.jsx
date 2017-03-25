df2084a3acffb5d9e1aca56fca69fef64b77ecc1
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-03-25 19:55:26 -0400

    Line up pictures and names in about us modal; set to hide on small devices

--- client/app/Description.jsx
+++ client/app/Description.jsx
@@ -1,36 +1,34 @@
 import React from 'react';
-import {Row, Col, Image, Button, Tooltip, Popover, OverlayTrigger} from 'react-bootstrap';
 
 //style={{zIndex: 2000}}
 //data-backdrop="false"
 var Description = () => (
-  <div>
+<div className="container-fluid">
+  <button type="button" className="aboutButton" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
+    About
+  </button>
 
-<button type="button" className="aboutButton" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
-  About
-</button>
+  <div className="modal fade show" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display: 'none'}} >
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">Description</h5>
+          <button  type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true"></span>
+          </button>
+        </div>
+        <div className="modal-body">
 
-<div className="modal fade show" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display: 'none'}} >
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Description</h5>
-        <button  type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"></span>
-        </button>
-      </div>
-      <div className="modal-body">
+        <p> This is where we can put images and description </p>
 
-      <p> This is where we can put images and description </p>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
 
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-
+        </div>
       </div>
     </div>
   </div>
-</div>
 </div>
   )
 
